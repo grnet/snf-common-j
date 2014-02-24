cf87b0d2ecbf3019aa0c113e5385c654c134a13a
Christos KK Loverdos <loverdos@gmail.com>
2014-02-24 18:24:01 +0200

    WIP CDMI support

---
+++ date/SimpleDateFormatParser.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012-2013 GRNET S.A. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed
+ * or implied, of GRNET S.A.
+ */
+
+package gr.grnet.common.date
+
+import java.text.{SimpleDateFormat, ParseException}
+
+/**
+ * A [[gr.grnet.common.date.DateParser]] based on [[java.text.SimpleDateFormat]].
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+final class SimpleDateFormatParser(val description: String) extends DateParser {
+  /**
+   * The description of this parser. This can be either a free text description or,
+   * in case of a [[java.text.DateFormat]]-based implementation, the format string.
+   */
+  val dateFormat = new SimpleDateFormat(description)
+
+
+  /**
+   * Tries to parse the given date.
+   * The implementation must not throw an [[java.lang.Exception]]. In particular,
+   * it must not throw a [[java.text.ParseException]], which is common in the case of a
+   * [[java.text.SimpleDateFormat]].
+   */
+  def parse(source: String) = {
+    try Some(dateFormat.parse(source))
+    catch {
+      case e: ParseException ⇒
+        None
+    }
+  }
+
+  override def toString = "%s(%s)".format(getClass.getSimpleName, description)
+}
